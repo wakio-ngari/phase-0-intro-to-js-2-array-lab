49c91712478404ba505ad34cda41685868cfb854
wakio-ngari <ngarijoyce017@gmail.com>
2024-11-29 12:59:10 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+
+let cats = ["Milo","Otis","Garfield"];
+ console.log(cats);
+     
+
+  function destructivelyAppendCat(name){
+    cats.push("Ralph");
+ }  
+ console.log(cats)
+  function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+  }
+     
+  function destructivelyRemoveLastCat(name){
+        cats.pop();
+     }
+           function destructivelyRemoveFirstCat(name){
+              cats.shift();
+           }
+
+              function appendCat(name){
+                return [...cats,name];
+              }
+                 function prependCat(name){
+                    return[name,...cats];
+                 }
+                
+                  
+                 function removeLastCat(name){
+                      return cats.slice(0,-1);
+               } 
+                   function removeFirstCat(){
+                    return cats.slice(1);
+                   }
